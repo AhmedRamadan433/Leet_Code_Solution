6abe776ca56b80c74124b4fbf5ed18efd81d7367
Ahmed ramadan <193283144+AhmedRamadan433@users.noreply.github.com>
2025-08-29 20:30:47 +0300

    [176] [Time Beats: 68.44%] - LeetPush

--- Second-Highest-Salary.sql
+++ Second-Highest-Salary.sql
@@ -1,9 +1,5 @@
-# Write your MySQL query statement below
-with test as ( select distinct salary from Employee  order by salary desc limit 2)
-
-select 
-case 
-WHEN COUNT(distinct salary) > 1 THEN MIN(salary)
-else null end as SecondHighestSalary 
-from test
- ;
+SELECT distinct salary
+as SecondHighestSalary
+FROM Employee
+ORDER BY salary DESC
+LIMIT 1, 1  ;
